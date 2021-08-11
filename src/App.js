bf32873e4bdd22275a230df905092506309791be
himansh9489 <30984612+himansh9489@users.noreply.github.com>
2021-08-11 17:56:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { useState } from "react";
+
+function Counter(props) {
+  const { startValue, key } = props;
+  const initialValue = 0;
+  if (startValue) {
+    initialValue = startValue;
+  }
+  const [num, setNum] = useState(initialValue);
+  return (
+    <div>
+      <h2>Counter</h2>
+      <div>{num}</div>
+      <button
+        onClick={function () {
+          setNum(num + 1);
+        }}
+      >
+        Inc
+      </button>
+    </div>
+  );
+}
+
 function App() {
   return (
     <>
       <h1>Welcome to React</h1>
+      <Counter startValue={12} key={1} />
     </>
   );
 }
